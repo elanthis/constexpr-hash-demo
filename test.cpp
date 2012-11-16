c59e42f4270c493494d504a0840425cd4d01e346
elanthis <sean@middleditch.us>
2012-11-15 21:31:18 -0800

    Added a license (CC0)

--- test.cpp
+++ test.cpp
@@ -1,3 +1,6 @@
+// This code is released under the terms of the "CC0" license.  Full terms and conditions
+// can be found at: http://creativecommons.org/publicdomain/zero/1.0/
+
 #include <iostream>
 
 // NOTE: hashing algorithm used is FNV-1a
